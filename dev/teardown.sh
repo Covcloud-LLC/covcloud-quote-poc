604c711120b4bfadf49bfbb29151beca62e522c5
Ken Brower <ken@covcloud.com>
2024-01-05 14:12:49 -0500

    Create dev

---
+++ dev/teardown.sh
@@ -0,0 +1,39 @@
+kubectl delete deployments mockrater-deployment
+sleep 1
+kubectl delete deployments mongo-deployment
+sleep 1
+kubectl delete deployments mongo-express-deployment
+sleep 1
+kubectl delete deployments zookeeper-deployment
+sleep 1
+kubectl delete deployments kafka-deployment
+sleep 1
+kubectl delete deployments schema-registry-deployment
+sleep 1
+kubectl delete deployments mockrater-deployment
+sleep 1
+kubectl delete service mockrater-loadbalancer
+sleep 1
+kubectl delete service mongo-service
+sleep 1
+kubectl delete service mongo-express-loadbalancer
+sleep 1
+kubectl delete service zookeeper-service
+sleep 1
+kubectl delete service kafka-service
+sleep 1
+kubectl delete service schema-registry-service
+sleep 1
+kubectl delete pvc kafka-pvc
+sleep 1
+kubectl delete pvc mongo-pvc
+sleep 1
+kubectl delete deployments quoterater-deployment
+sleep 1
+kubectl delete deployments quoteprocess-deployment
+sleep 1
+kubectl delete deployments/quotedetails-deployment
+sleep 1
+kubectl delete deployments/quotemanager-deployment
+sleep 1
+kubectl delete services quotemanager-loadbalancer
